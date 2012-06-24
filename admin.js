bfac637c3cde2eacce5f90b5ce05da77784f834f
Patrick Rauscher <prauscher@ohai.su>
2012-06-24 21:47:16 +0000

    added combine.js, which allows password authentification for admin-actions. usefull for small congresses

--- admin.js
+++ admin.js
@@ -12,23 +12,9 @@ app.get('/', function (req, res) {
 	res.render('admin');
 });
 
-app.post('/beamer-identify', routes.beamer.identify);
-
-app.put('/agenda/:slideid/save', routes.agenda.save);
-app.post('/agenda/:slideid/delete', routes.agenda.delete);
-app.post('/agenda/:slideid/move', routes.agenda.move);
-
-app.put('/beamer/:beamerid/save', routes.beamer.save);
-app.post('/beamer/:beamerid/delete', routes.beamer.delete);
-app.post('/beamer/:beamerid/showtimer', routes.beamer.showTimer);
-app.post('/beamer/:beamerid/hidetimer', routes.beamer.hideTimer);
-app.post('/beamer/:beamerid/flash', routes.beamer.flash);
-
-app.put('/timers/:timerid/save', routes.timers.save);
-app.post('/timers/:timerid/delete', routes.timers.delete);
-app.post('/timers/:timerid/start', routes.timers.start);
-app.post('/timers/:timerid/pause', routes.timers.pause);
-app.post('/timers/:timerid/stop', routes.timers.stop);
+app.addAdminRoutes(function (req, res, route) {
+	route(req, res);
+});
 
 console.dir(config);
 
---
+++ combined.js
@@ -0,0 +1,52 @@
+/**
+ * Module dependencies.
+ */
+
+var config = require('./config.js'),
+    routes = require('./routes'),
+    app = require('./app.js');
+
+// Routes
+
+app.get('/', function (req, res) {
+	res.render('clientOverview');
+});
+
+app.get('/login', function (req, res) {
+	res.render('login');
+});
+
+app.post('/login', function (req, res) {
+	if (req.body.password == config.password) {
+		req.session.password = req.body.password;
+		res.send({ success: true });
+	} else {
+		res.send({ success: false });
+	}
+});
+
+function checkPermissions(req, res, route) {
+	if (req.session.password == config.password) {
+		route(req, res);
+	} else {
+		res.redirect('/login');
+	}
+}
+
+app.get('/admin', function (req, res) {
+	checkPermissions(req, res, function () {
+		res.render('admin');
+	});
+});
+
+app.addAdminRoutes(checkPermissions);
+
+console.dir(config);
+
+app.listen(config.port, config.host, function() {
+        if (process.getuid() == 0) {
+		process.setgid(config.setgid);
+		process.setuid(config.setuid);
+	}
+	console.log("Express server listening on http://localhost:%d/ in mode %s", app.address().port, app.settings.env);
+});
