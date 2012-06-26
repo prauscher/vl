fa6c03c6b75e3c02aaa5a9bf59f20be57bb286f2
Patrick Rauscher <prauscher@ohai.su>
2012-06-26 13:11:47 +0000

    bugfix: hide miniColor-fields when matching modal gets closed

--- public/javascript/admin/index.js
+++ public/javascript/admin/index.js
@@ -1,3 +1,9 @@
 function generateID() {
 	return Math.random().toString(36).substring(2);
 }
+
+$(function () {
+	$(".modal").on("hide", function() {
+		$(this).find(".miniColors").miniColors("destroy");
+	});
+});
