1a0c5c0586de251f5e46a8dd731627a6a5f5910f
Patrick Rauscher <prauscher@ohai.su>
2012-07-08 13:42:24 +0000

    less code in routes

--- routes/motionclasses.js
+++ routes/motionclasses.js
@@ -1,27 +1,5 @@
-exports.save = function (req, res) {
-	var motionclass = req.body.motionclass;
+var backendRouter = require('./backend.js');
 
-	backend.motionclasses.exists(req.params.motionclassid, function (exists) {
-		if (! exists) {
-			backend.motionclasses.add(req.params.motionclassid, motionclass, function() {
-				res.send(200);
-			});
-		} else {
-			backend.motionclasses.save(req.params.motionclassid, motionclass, function() {
-				res.send(200);
-			});
-		}
-	});
-}
-
-exports.move = function (req, res) {
-	backend.motionclasses.move(req.params.motionclassid, req.body.parentid, req.body.position, function () {
-		res.send(200);
-	});
-}
-
-exports.delete = function (req, res) {
-	backend.motionclasses.delete(req.params.motionclassid, function () {
-		res.send(200);
-	});
-}
+exports.save = backendRouter.generateSave(backend.motionclasses, "motionclassid", "motionclass");
+exports.move = backendRouter.generateMove(backend.motionclasses, "motionclassid", "parentid", "position");
+exports.delete = backendRouter.generateDelete(backend.motionclasses, "motionclassid");
--- routes/motions.js
+++ routes/motions.js
@@ -1,31 +1,9 @@
+var backendRouter = require('./backend.js');
+
 exports.showMotion = function (req, res) {
 	res.render('showProjector', { motionid : req.params.motionid });
 }
 
-exports.save = function (req, res) {
-	var motion = req.body.motion;
-
-	backend.motions.exists(req.params.motionid, function (exists) {
-		if (! exists) {
-			backend.motions.add(req.params.motionid, motion, function() {
-				res.send(200);
-			});
-		} else {
-			backend.motions.save(req.params.motionid, motion, function() {
-				res.send(200);
-			});
-		}
-	});
-}
-
-exports.move = function (req, res) {
-	backend.motions.move(req.params.motionid, req.body.classid, req.body.position, function () {
-		res.send(200);
-	});
-}
-
-exports.delete = function (req, res) {
-	backend.motions.delete(req.params.motionid, function () {
-		res.send(200);
-	});
-}
+exports.save = backendRouter.generateSave(backend.motions, "motionid", "motion");
+exports.move = backendRouter.generateMove(backend.motions, "motionid", "classid", "position");
+exports.delete = backendRouter.generateDelete(backend.motions, "motionid");
