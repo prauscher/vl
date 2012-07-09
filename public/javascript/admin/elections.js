2f80b5cf94e536893c33c241e4dfca90911633be
Patrick Rauscher <prauscher@ohai.su>
2012-07-09 11:48:47 +0000

    removed unused color-field

--- public/javascript/admin/elections.js
+++ public/javascript/admin/elections.js
@@ -4,8 +4,7 @@ $(function () {
 	var showElectionOptions = generateShowOptionsModal({
 		modal : "#elections #election-options",
 		fields : [
-			{ property : "title", field : "#title", type : "text" },
-			{ property : "color", field : "#color", type : "color" }
+			{ property : "title", field : "#title", type : "text" }
 		],
 		saveCallback : apiClient.saveElection,
 		deleteCallback : apiClient.deleteElection
@@ -13,12 +12,10 @@ $(function () {
 
 	apiClient.on("initElection", function (electionid, election) {
 		$("#elections #elections").append($("<tr>").attr("id", "election-" + electionid)
-			.append($("<td>").append($("<img>").addClass("color").attr("src","/images/empty.gif")))
 			.append($("<td>").addClass("title")) );
 	});
 
 	apiClient.on("updateElection", function (electionid, election) {
-		$("#elections #election-" + electionid + " .color").css("background-color", election.color);
 		$("#elections #election-" + electionid + " .title").text(election.title);
 
 		$("#elections #election-" + electionid + " .title").unbind("click").css("cursor", "pointer").click(function () {
