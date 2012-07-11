c540eab15bc09451d208b2036a75d7acc97b5282
Benjamin Richter <br@waldteufel.eu>
2012-07-12 01:41:25 +0200

    paragraph numbers for motion text
    closes #16

--- public/javascript/viewer/projectors.js
+++ public/javascript/viewer/projectors.js
@@ -82,6 +82,15 @@ function clearView() {
 	$('#content .content-ballot').hide();
 }
 
+function numberLines(text) {
+	var lines = text.split("\n");
+	var tag = $("<ol>").addClass("line-numbers");
+	$.each(lines, function(idx, line) {
+	        tag.append($("<li>").append($("<span>").text(line)));
+	});
+	return tag;
+}
+
 function showView(type, options) {
 	clearView();
 
@@ -99,7 +108,7 @@ function showView(type, options) {
 	if (type == "motion") {
 		$('#title').text(options.motion.title);
 		$(".motionid").text(options.motionid);
-		$(".motion-text").text(options.motion.text);
+		$(".motion-text").append(numberLines(options.motion.text));
 		$(".motion-argumentation").text(options.motion.argumentation);
 		$(".motion-submitter").text(options.motion.submitter);
 		$(".motion-status *").hide();
