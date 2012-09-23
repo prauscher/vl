579c732361bc69fcd4a89e9bb80abd759d9d71a6
Patrick Rauscher <prauscher@ohai.su>
2012-09-23 02:02:55 +0000

    added missing file 2 commits ago

---
+++ public/javascript/showTimer/currenttime.js
@@ -0,0 +1,19 @@
+// vim:noet:sw=8:
+
+function updateCurrentTime() {
+	var now = new Date();
+	var hours = now.getHours();
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	var minutes = now.getMinutes();
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	$("#currentTime").text("⌚ " + hours + ":" + minutes);
+}
+
+$(function () {
+	updateCurrentTime();
+	window.setInterval(updateCurrentTime, 1000);
+});
