976f8a2ac5956620b7b23ef32d19443bfe806dce
Patrick Rauscher <prauscher@ohai.su>
2012-10-27 21:57:39 +0000

    add callback to backendRouter.generateSave (useful for #40)

--- routes/backend.js
+++ routes/backend.js
@@ -1,16 +1,18 @@
 // vim:noet:sw=8:
 
-exports.generateSave = function (backend, idfield, itemfield) {
+exports.generateSave = function (backend, idfield, itemfield, callback) {
 	return function (req, res) {
 		var item = req.body[itemfield];
 
 		backend.exists(req.params[idfield], function (exists) {
 			if (! exists) {
 				backend.add(req.params[idfield], item, function() {
+					callback && callback(req.params[idfield], item, true);
 					res.send(200);
 				});
 			} else {
 				backend.save(req.params[idfield], item, function() {
+					callback && callback(req.params[idfield], item, false);
 					res.send(200);
 				});
 			}
